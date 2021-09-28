c976ca282bc87c75ae77623ba561b38e167f8e9d
yifguoMSFT <yifguo@microsoft.com>
2021-09-27 17:09:20 -0700

    update

--- Angular/redeploy.bat
+++ Angular/redeploy.bat
@@ -1,2 +1,2 @@
-kubectl scale --replicas=0 deployment console-ui-deployment
-kubectl scale --replicas=1 deployment console-ui-deployment
+kubectl scale --replicas=0 deployment console-ui
+kubectl scale --replicas=1 deployment console-ui
